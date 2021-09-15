46ca76efd8c6e564e821c6e8020a34685e32a48c
enjee1 <22105805+enjee1@users.noreply.github.com>
2021-09-14 20:23:09 -0400

    Refactored determineWinner() in Game class, adjusted spacing in launchGame() in Menu class

--- src/com/company/Game.java
+++ src/com/company/Game.java
@@ -4,76 +4,6 @@ import java.util.ArrayList;
 
 public class Game {
 
-/*    private ArrayList<Player> players = new ArrayList<Player>();
-    private ArrayList<Die> dice = new ArrayList<Die>();
-    private int numRounds;
-    private int numDice;
-    private ArrayList<String> diceValues;
-    private Player winner;
-
-
-    public Game(int numOfPlayers, int numRounds, int numDice) {
-        this.numRounds = numRounds;
-        this.numDice = numDice;
-        generatePlayers(numOfPlayers);
-        generateDice(numDice);
-        startGame();
-    }
-
-    private void playerTurn(Player player) {
-        boolean playerInput;
-        int roundScore = 0;
-        diceValues = new ArrayList<String>();
-
-        System.out.print(player.getName() + ", it's your turn.\nPress Enter to roll your die/dice. ");
-        playerInput = CLI.readEnterKey();
-
-        while (playerInput) {
-            for (int i = 0; i < numRounds ; i++) {
-                for (Die die : dice) {
-                    die.roll();
-                    roundScore += die.getValue();
-//                    diceValues.add(die.toString());
-                }
-                player.setScore(roundScore);
-            }
-            System.out.println(player.getName() + ", you rolled: " + showDiceRolls(diceValues));
-            playerInput = false;
-        }
-    }
-
-    public void startGame() {
-        for (Player player : players) {
-            playerTurn(player);
-        }
-
-    }
-
-    private void generatePlayers(int numPlayers) {
-        for (int i = 0; i < numPlayers; i++) {
-            String name = CLI.getString("Player " + (i + 1) + ", enter your name, then press Enter.\nName: ");
-            Player newPlayer = new Player(name);
-            players.add(newPlayer);
-        }
-    }
-
-    private void generateDice(int numDice) {
-        for (int i = 0; i < numDice; i++) {
-            Die die = new Die();
-            dice.add(die);
-        }
-    }
-
-    private String showDiceRolls(ArrayList<String> dice) {
-        String rolls = "";
-        for (int i = 0; i < dice.size() ; i++) {
-
-            *//*if (i != dice.size()) rolls += dice.get(i).toString() + ", ";
-            else rolls += dice.get(i).toString();*//*
-        }
-
-        return rolls;
-    }*/
 
     private ArrayList<Player> players = new ArrayList<Player>();
     private int numRounds;
@@ -149,14 +79,16 @@ public class Game {
     private void determineWinner() {
         int highestScore = 0;
 
-
-        for (int i = 0; i < players.size(); i++) {
-            if (players.get(i).getScore() >= highestScore) { highestScore = players.get(i).getScore(); }
-        }
-
         for (Player player : players) {
-            if (player.getScore() == highestScore) winner.add(player);
+            if (player.getScore() > highestScore) {
+                highestScore = player.getScore();
+                winner.clear();
+                winner.add(player);
+            } else if (player.getScore() == highestScore) {
+                winner.add(player);
+            }
         }
+
         System.out.println(showWinner(winner));
     }
 
--- src/com/company/Menu.java
+++ src/com/company/Menu.java
@@ -71,9 +71,6 @@ public class Menu {
 
             Game game = new Game(numPlayers, numRounds, numDice);
         }
+
     }
-
-
-
-
 }
